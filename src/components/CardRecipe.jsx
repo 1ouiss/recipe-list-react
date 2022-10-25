3b5cbce93f92f76cdaec91384c299c06c5043c9a
Louis BOUET <louisbouet@MacBook-Pro-de-Louis.local>
2022-10-25 17:55:30 +0200

    add card recipe

--- src/components/CardRecipe.jsx
+++ src/components/CardRecipe.jsx
@@ -1,10 +1,26 @@
 const CardRecipe = ({recipe}) => {
+    const { title, ingredients, instructions } = recipe;
+
+    const sliptList = (text) => {
+        return text.split(',').map((item, index) => (
+            <li key={index}>{item}</li>
+        ));
+    };
     return ( 
         <li>
-            <h2>{recipe.title}</h2>
-            <p>{recipe.ingredients}</p>
-            <p>{recipe.instructions}</p>
-            <p>{recipe.pseudo}</p>
+            <h2>{title}</h2>
+            <div>
+                <h3>Ingrédients</h3>
+                <ul>
+                    {sliptList(ingredients)}
+                </ul>
+            </div>
+            <div>
+                <h3>Instructions</h3>
+                <ul>
+                    {sliptList(instructions)}
+                </ul>
+            </div>
         </li>
      );
 }
